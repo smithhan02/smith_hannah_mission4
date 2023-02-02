b4d1fb441fd4af0271bee023106e71a4de71c45e
Hannah Smith <hls68@byu.edu>
2023-02-02 15:55:53 -0700

    new

--- smith_hannah_mission4/Startup.cs
+++ smith_hannah_mission4/Startup.cs
@@ -16,12 +16,16 @@ namespace smith_hannah_mission4
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-           
+            if (env.IsEnvironment("Development"))
+            {
+                app.UseDeveloperExceptionPage();
+            }
            app.UseStaticFiles();
 
            app.UseRouting();
